ea1e5470af97fced172374e706cef4f676f29dc8
robby7370 <65488276+robby7370@users.noreply.github.com>
2022-03-30 09:45:03 +0700

    Chapter 8 - UserDataManager.HasResources Not Found
    UserDataManager.HasResources - Not Found

---
+++ Assets/Scripts/LoadingController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingController : MonoBehaviour
+{
+    private void Start ()
+
+    {
+
+        UserDataManager.Load ();
+
+        SceneManager.LoadScene (1);
+
+    }
+}
